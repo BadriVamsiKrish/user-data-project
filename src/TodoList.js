a86e54cc0260d8cfb628182e4ccb25859309d591
BadriVamsiKrish <www.1203badrivamsi@.com>
2023-05-01 08:15:30 +0530

    first 3hrs project

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,34 @@
 import React from 'react'
 
-const TodoList = ({todolist,deleteHandler}) => {
+const TodoList = ({todolist,deleteHandler,Table2,Table1,Table3,setTable1,setTable2,setTable3}) => {
+  
     return (
         <div>
-            {todolist.map((todo,index) =>
-            <div key={index}>
-                <h5>{todo} &nbsp; <button onClick={() => deleteHandler(index)}>Delete</button></h5>
-            </div>)}
+          <h1>Orders</h1>{
+            console.log(Table2)
+          }
+            
+            <h2>Table1</h2>
+            {
+              Table1.map((todo,index) =>
+              <div key={index}>
+                  <h3>{todo}<button onClick={() => deleteHandler(index,Table1,setTable1)}>Delete</button></h3>
+              </div>)
+            }
+            <h2>Table2</h2>
+            {
+              Table2.map((todo,index) =>
+              <div key={index}>
+                  <h3>{todo}<button onClick={() => deleteHandler(index,Table2,setTable2)}>Delete</button></h3>
+              </div>)
+            }
+            <h2>Table3</h2>
+            {
+              Table3.map((todo,index) =>
+              <div key={index}>
+                  <h3>{todo}<button onClick={() => deleteHandler(index,Table3,setTable3)}>Delete</button></h3>
+              </div>)
+            }
         </div>
     )
 }
